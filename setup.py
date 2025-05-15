23d92f89de1b6275da00bef58697cadcbbcb998a
Talha Sarfraz <138433391+talha-11-11@users.noreply.github.com>
2025-05-15 16:40:50 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,35 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='dataqualitychecker',  
+    name='dataqualitychecker',  # Must be unique on PyPI
     version='0.1.0',
     author='Talha Sarfraz',
     author_email='talhasarfraz29@gmail.com',
-    description='A Python package for quick data quality profiling with visualization and imputation suggestions',
+    description='A Python package for automated data profiling, missing value detection, outlier identification, and imputation suggestions.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/talha-11-11/dataqualitychecker',
     packages=find_packages(),
     classifiers=[
-        'Programming Language :: Python :: 3',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     install_requires=[
-        'pandas',
-        'seaborn',
-        'matplotlib',
+        'pandas>=1.1',
+        'matplotlib>=3.3',
+        'seaborn>=0.11',
+        'numpy>=1.19'
     ],
+    project_urls={
+        'Source': 'https://github.com/talha-11-11/dataqualitychecker',
+        'Tracker': 'https://github.com/talha-11-11/dataqualitychecker/issues',
+    },
+    include_package_data=True,
 )
