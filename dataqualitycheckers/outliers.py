eed90850cf552c8a0b95663415d399ff42b910e1
Talha Sarfraz <138433391+talha-11-11@users.noreply.github.com>
2025-05-15 16:36:02 +0500

    Create outliers.py

---
+++ dataqualitycheckers/outliers.py
@@ -0,0 +1,6 @@
+def detect_outliers_iqr(df, column):
+    Q1 = df[column].quantile(0.25)
+    Q3 = df[column].quantile(0.75)
+    IQR = Q3 - Q1
+    outliers = df[(df[column] < Q1 - 1.5 * IQR) | (df[column] > Q3 + 1.5 * IQR)]
+    return outliers
